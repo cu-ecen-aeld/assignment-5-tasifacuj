ac6204590bc29c32dae675b8e176a8d15ee1adfa
Tez <tasifacij@gmail.com>
2024-09-23 15:24:54 +0200

    Assignment-8 fix comments.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '8ff6a910661658eecec9c98ca8eb4c4ed0903dea'
+AESD_ASSIGNMENTS_VERSION = '2ccf949e1446b79a4e3f749e5cee5d705dd540e0'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
